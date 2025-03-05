332ed724352a3fdee5c311eac3c47fff9090c13c
César Faustino <tarefas.fatec@gmail.com>
2025-03-05 19:23:50 -0300

    fix ci test workflow to skip tailwind build in the test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -4,6 +4,7 @@
 # and recreated between test runs. Don't rely on the data there!
 
 Rails.application.configure do
+  config.assets.tailwind_build = ENV["SKIP_TAILWIND_BUILD"] != "true"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # While tests run files are not watched, reloading is not necessary.
